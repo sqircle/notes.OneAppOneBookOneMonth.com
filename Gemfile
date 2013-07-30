7cb3f77d28a44be39694c690031f773f97bbf544
K-2052 <k@2052.me>
2013-07-30 08:30:46 -0700

    updates

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
+ruby '2.0.0'
 
 gem 'middleman'
 gem 'middleman-blog'
-
-gem 'builder'
+gem 'rack-contrib'
 gem 'slim'
+gem 'puma'
--- config.rb
+++ config.rb
@@ -15,7 +15,7 @@ end
 
 ###
 # Pages
-#333
+###
 page "/feed.xml",   :layout => false
 page "/about.html", :layout => 'page'
 
@@ -28,22 +28,14 @@ set :images_dir, 'images'
 ###
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
   
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
   
   # Enable cache buster
-  # activate :cache_buster
+  activate :cache_buster
   
   # Use relative URLs
-  # activate :relative_assets
-  
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-  
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
+  activate :relative_assets
 end
